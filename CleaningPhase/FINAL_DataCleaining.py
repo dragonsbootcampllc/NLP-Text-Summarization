f8f01719425732bfbe9c7ce6722242e1a7e99e08
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2023-05-12 06:27:28 +0300

    add headline to be cleaned too

--- CleaningPhase/FINAL_DataCleaining.py
+++ CleaningPhase/FINAL_DataCleaining.py
@@ -6,8 +6,6 @@ import re
 from nltk.corpus import stopwords
 import nltk
 
-
-
 with open('data.csv', 'rb') as f:
     result = chardet.detect(f.read())
 encoding = result['encoding']
@@ -17,7 +15,7 @@ with open("data.csv", 'r', encoding=encoding) as f:
 
 
 def sellectDataWeNeed(data):
-    data = data[['text', 'ctext']]
+    data = data[['text', 'ctext', 'headlines']]
     return data
 
 
@@ -30,6 +28,8 @@ def checkData(data):
 
 def removeDuplicate(data):
     data.drop_duplicates(subset=['text'], inplace=True)
+    data.drop_duplicates(subset=['ctext'], inplace=True)
+    data.drop_duplicates(subset=['headlines'], inplace=True)
     return data
 
 
@@ -37,12 +37,14 @@ def TransformToLowercase(Data_sellected):
     # Try using .loc[row_indexer,col_indexer] = value instead
     Data_sellected.loc[:, 'text'] = Data_sellected['text'].apply(lambda x: x.lower())
     Data_sellected.loc[:, 'ctext'] = Data_sellected['ctext'].apply(lambda x: x.lower())
+    Data_sellected.loc[:, 'headlines'] = Data_sellected['headlines'].apply(lambda x: x.lower())
     return Data_sellected
 
 
 def removePunctuation(data):
     data.loc[:, 'text'] = data['text'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
     data.loc[:, 'ctext'] = data['ctext'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+    data.loc[:, 'headlines'] = data['headlines'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
     return data
 
 
@@ -50,12 +52,15 @@ def removeStopWords(data):
     stop_words = set(stopwords.words('english'))
     data['text'] = data['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
     data['ctext'] = data['ctext'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
+    data['headlines'] = data['headlines'].apply(
+        lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
     return data
 
 
 def removeNonString(data):
     data = data[data['text'].apply(lambda x: isinstance(x, str))]
     data = data[data['ctext'].apply(lambda x: isinstance(x, str))]
+    data = data[data['headlines'].apply(lambda x: isinstance(x, str))]
     return data
 
 
@@ -67,23 +72,26 @@ def removeNullValues(data):
 def nonEnglishValues(data):
     data = data[data['text'].apply(lambda x: x.isascii())]
     data = data[data['ctext'].apply(lambda x: x.isascii())]
+    data = data[data['headlines'].apply(lambda x: x.isascii())]
     return data
 
 
 def removeSingleChar(data):
     data['text'] = data['text'].apply(lambda x: re.sub(r'\s+[a-zA-Z]\s+', ' ', x))
     data['ctext'] = data['ctext'].apply(lambda x: re.sub(r'\s+[a-zA-Z]\s+', ' ', x))
+    data['headlines'] = data['headlines'].apply(lambda x: re.sub(r'\s+[a-zA-Z]\s+', ' ', x))
     return data
 
 
 def removeExtraSpaces(data):
     data['text'] = data['text'].apply(lambda x: re.sub(' +', ' ', x))
     data['ctext'] = data['ctext'].apply(lambda x: re.sub(' +', ' ', x))
+    data['headlines'] = data['headlines'].apply(lambda x: re.sub(' +', ' ', x))
     return data
 
 
 def renameColumns(data):
-    data.rename(columns={'text': 'summary', 'ctext': 'text'}, inplace=True)
+    data.rename(columns={'text': 'summary', 'ctext': 'text', 'headlines': 'query'}, inplace=True)
     print(data.head())
     print(data.info())
     print(data.describe())
@@ -128,3 +136,5 @@ def main(data):
 
 
 main(data)
+
+# Tokinzation&Lemmatization
